bacd4c5c8edebbf59e697f26c63cfa25168a28f0
Ishan7015 <ip.contactto@gmail.com>
2024-08-05 02:12:34 +0530

    Automated year, instead of hard coding it

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,17 +5,17 @@ import { SiLeetcode } from 'react-icons/si';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-      <a href='https://github.com/Ishan7015'>
+      <a href='https://github.com/Ishan7015' target='_blank'>
         <div >
             <BsGithub />
         </div>
       </a>
-      <a href='https://www.linkedin.com/in/ishanpandey2001/'>
+      <a href='https://www.linkedin.com/in/ishan017/' target='_blank'>
         <div>
           <BsLinkedin />
         </div>
       </a>
-      <a href='https://leetcode.com/IshanDev/'>
+      <a href='https://leetcode.com/IshanDev/' target='_blank'>
         <div>
           <SiLeetcode />
         </div>
--- src/wrapper/AppWrap.jsx
+++ src/wrapper/AppWrap.jsx
@@ -4,13 +4,16 @@ import { NavigationDots, SocialMedia } from '../components';
 
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
+
+  const currentYear = new Date().getFullYear();
+
     return (
         <div id={idName} className={`app__container ${classNames}`}>
             <SocialMedia />
             <div className='app__wrapper app__flex'>
                 <Component />
                 <div className="copyright">
-                    <p className="p-text">2022 ISHAN</p>
+                    <p className="p-text">{currentYear} ISHAN</p>
                     <p className="p-text">All Right Reserved</p>
                 </div>
             </div>
